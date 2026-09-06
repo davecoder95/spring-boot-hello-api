3c97e44bb82f3f5698e7c6417a2d92a7fc274345
davecoder95 <debashr95@gmail.com>
2026-09-06 14:34:50 -0700

    new changes

--- src/main/java/com/example/dave/demo/controller/ProductController.java
+++ src/main/java/com/example/dave/demo/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.example.dave.demo.controller;
 import com.example.dave.demo.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,10 @@ public class ProductController {
     @GetMapping("/products/{id}")
 public Product getProductById(@PathVariable Long id) {
     return new Product(id, "Laptop", 999.99);
+}
+
+@PostMapping("/products")
+public Product createProduct(@RequestBody Product product) {
+    return product;
 }
 }

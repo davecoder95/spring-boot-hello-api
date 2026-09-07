b371ed03335e01c703292ed77985141a9f428ba1
davecoder95 <debashr95@gmail.com>
2026-09-06 22:40:03 -0700

    GitHub Commit Complete REST API before database

--- src/main/java/com/example/dave/demo/model/Product.java
+++ src/main/java/com/example/dave/demo/model/Product.java
@@ -1,9 +1,19 @@
 package com.example.dave.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private double price;
 
     public Product() {
